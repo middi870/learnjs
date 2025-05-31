3a7347e8e62179b6a1794674128c0a81d1bbf2d3
middi870 <midwayworld19@gmail.com>
2025-05-31 18:54:11 +0000

    ff

---
+++ 01_learn/datatypes_summary.js
@@ -0,0 +1,35 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, Symbol, BigInt, undefined, null.
+
+// JS is dynamically typed language
+const score = 100;
+const scoreValue = 99.8;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+console.log(id === anotherId);
+
+const bigNumber = 1234567891234569789123546789n
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraja", "doga"];
+
+let myObj = {
+    name: "middi",
+    age: "16",
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof myFunction);
