c891bd0e6faef42060c0994191dd526a7a92750e
middi870 <midwayworld19@gmail.com>
2025-05-31 18:00:15 +0000

    conversion of data types

---
+++ 01_learn/03_conversionOperation.js
@@ -0,0 +1,33 @@
+// let score = "86ddgs";
+
+// console.log(typeof (score));
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // NaN:- Not-a-Number
+
+// '33' => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+// ----------------
+// let isLoggedIn = 1;
+
+// let booleanIsLoggedin = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedin)
+
+// //1 => true; 0 => false
+// // "" => false
+// // "string" => true
+// -----------------
+
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
