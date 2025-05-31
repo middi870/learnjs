a80cd87f1550555814399ec174512d32d32ff9dd
middi870 <midwayworld19@gmail.com>
2025-05-31 17:11:51 +0000

    f

---
+++ 01_learn/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553;
+let accountEmail = "jhifg@google.com";
+var accountPassword = "123456"; // don't use. It doesn't have scope.
+accountCity = "Jaipur";
+let accountState;
+
+/*
+accountId = 2; // we can't change the constant variable.
+
+console.log(accountId);
+// --------------------
+*/
+
+accountEmail = "hc@gmail.com";
+accountPassword = "123245678";
+accountCity = "Bengal";
+
+console.table({accountId, accountEmail, accountPassword, accountCity, accountState});
