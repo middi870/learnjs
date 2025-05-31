c4318f061df50eb6e7aace544e96857e4df7e2e3
middi870 <midwayworld19@gmail.com>
2025-05-31 17:43:08 +0000

    f

---
+++ 01_learn/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treats all JS code as newer version
+
+// alert("Hi") // we are using nodejs, not the browser
+
+let name = "Middi";
+let age = 18;
+let isLoggedIn = false;
+let state;
+
+// -------------
+// * primitive Data types * :-
+// number => 2 to the power 53
+// bigint
+// string => "" prefered
+// boolean => true/false
+// null => standalone
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
